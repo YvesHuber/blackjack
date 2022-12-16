d82349a923ade6cf24ba00b44b9f76c940ab4efd
guywithoutlif3 <72743368+guywithoutlif3@users.noreply.github.com>
2022-12-16 10:26:47 +0100

    style changes
    i did style

--- src/Cards.js
+++ src/Cards.js
@@ -1,8 +1,10 @@
+/* eslint-disable jsx-a11y/alt-text */
 import './App.css';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
-import {useEffect, useState} from 'react';
-
+import { useEffect, useState } from 'react';
+import "./Cards.css";
+import draw from "./Download__2_-removebg-preview.png"
 export default function Cards() {
     const [deck, setDeck] = useState([]);
     const [deckID, setDeckID] = useState();
@@ -20,6 +22,7 @@ export default function Cards() {
     }
 
     function getCard(setCard, currentCards) {
+        document.getElementById("vid").play();
         fetch('https://deckofcardsapi.com/api/deck/' + deckID + '/draw/?count=1')
             .then(response => response.json())
             .then(data => setCard(currentCards => [...currentCards, data.cards[0]]))
@@ -38,11 +41,11 @@ export default function Cards() {
             .catch((error) => {
                 console.error('Error:', error);
             });
-            console.log(card)
-            
+        console.log(card)
+
     }
 
-    function startGame(){
+    function startGame() {
         getCard(setPlayercard, playercard)
         getCard(setPlayercard, playercard)
         getHiddenCard(setDealercard, dealercard)
@@ -55,60 +58,77 @@ export default function Cards() {
     }
 
     useEffect(() => {
+        document.getElementById("vid").play();
         getDeck()
-    }, []);  
+    }, []);
     useEffect(() => {
+
         console.log(deckID)
-        if(deckID != undefined){
+        if (deckID != undefined) {
             startGame()
         }
-    }, [deckID]);              
+    }, [deckID]);
     //Ruft beim starten der Seite die function getDeck () auf
 
     useEffect(() => {
+
         console.log(playercard)
     }, [playercard]);     //wird erst gemacht, wenn playercard gemacht/abgefüllt wurde
-    
+
     useEffect(() => {
         console.log(dealercard)
     }, [dealercard]);     //wird erst gemacht, wenn playercard gemacht/abgefüllt wurde
 
     return (<div className="App">
-            <header className="App-header">
-                {/*{card && <img src={card.image}></img>}      dies hat nur funktioniert als card oben noch kein Array war*/}
-                <Grid container rowSpacing={3} alignItems="flex-start" justifyContent="center">
-                    <Grid>
-                        <h2>Dealer</h2>
-                    </Grid>
-                <Grid container spacing={5} alignItems="center" justifyContent="center">
-                        {dealercard.map((c) => <Grid item>
-                            <img height={"80vh"} src={c.image}></img>
-                            <p>{c.value}</p>
-                        </Grid>)}
-                    </Grid>
+        <header className="App-header">
+            <audio id='vid'autoplay controls loop >
+                <source  src="/mudick.mp3" type="audio/ogg" />
+            </audio>
+
+            <script>
+            </script>
+            {/*{card && <img src={card.image}></img>}      dies hat nur funktioniert als card oben noch kein Array war*/}
+            <Grid container rowSpacing={3} alignItems="flex-start" justifyContent="center">
 
 
-                    <Grid>
-                        <h2>Player</h2>
-                    </Grid>
-                    <Grid container spacing={5} alignItems="center" justifyContent="center">
-                        {playercard.map((c) => <Grid item>
-                            <img height={"80vh"} src={c.image}></img>
-                            <p>{c.value}</p>
-                        </Grid>)}
-                    </Grid>
-                    <Grid item>
-                        <Button variant="contained" onClick={(e) => getCard(setPlayercard, playercard)}>Player</Button>
-                    </Grid>
-
-
-                    <Grid item>
-                        <Button variant="contained" onClick={clearCards}>Restart</Button>
-                    </Grid>
+                <Grid>
+                    <h2>Dealer</h2>
 
                 </Grid>
-            </header>
-        </div>
+                <Grid container spacing={5} alignItems="center" justifyContent="center">
+                    {dealercard.map((c) => <Grid item>
+                        <img height={"80vh"} src={c.image}></img>
+                        <p>{c.value}</p>
+                    </Grid>)}
+                </Grid>
+
+
+                <Grid>
+                    <h2>Player</h2>
+                </Grid>
+
+                <Grid container spacing={5} alignItems="center" justifyContent="center">
+
+                    {playercard.map((c) => <Grid item >
+                        <img class="cards" height={"80vh"} src={c.image}></img>
+                        <p>{c.value}</p>
+                    </Grid>
+                    )}
+                    <img height={"150vh"} src='/dick2.png' class="dick"></img>
+
+                </Grid>
+                <Grid item>
+                    <Button class="PlayerBtn" variant="contained" onClick={(e) => getCard(setPlayercard, playercard)}><img class="drawImg" src={draw} />Player</Button>
+                </Grid>
+
+
+                <Grid item>
+                    <Button class="RestartBtn" variant="contained" onClick={clearCards}>Restart</Button>
+                </Grid>
+
+            </Grid>
+        </header>
+    </div>
 
     )
 }
